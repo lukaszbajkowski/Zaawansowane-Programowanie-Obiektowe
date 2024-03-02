cbfa0d19e3e14bee979f4ea293af3e30684fbce9
mixvert <bajkowski1e@gmail.com>
2024-03-02 17:07:36 +0100

    Addition of task 3

---
+++ src/zadanie3/CarSpeed.java
@@ -0,0 +1,17 @@
+package zadanie3;
+
+public class CarSpeed {
+    private final Car car;
+
+    public CarSpeed(Car car) {
+        this.car = car;
+    }
+
+    public int getMaxSpeed() {
+        return car.getMaxSpeed();
+    }
+
+    public void showCarName() {
+        System.out.println(car.getCarName());
+    }
+}
